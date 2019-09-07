a812e2241295ecaae21c3271af02229bab9848f4
fyxiaoyaoFXY <1653771508@qq.com>
2019-09-07 20:45:43 +0800

    three

--- （2）NodeJS全局API/five.js
+++ （2）NodeJS全局API/five.js
@@ -1,3 +1,3 @@
-console.log(process.pid);
-console.log(process.platform);
-console.log(process.argv);
+console.log(process.pid);//进程的PID
+console.log(process.platform);//进程运行的操作系统平台
+console.log(process.argv);//当前进程的文件路径和当前工作目录
