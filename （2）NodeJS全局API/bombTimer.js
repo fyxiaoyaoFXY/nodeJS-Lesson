30c5756cdf57147e97dbd478f0ad1a4e9ea1493d
fyxiaoyaoFXY <1653771508@qq.com>
2019-09-09 14:33:44 +0800

    three2

--- （2）NodeJS全局API/bombTimer.js
+++ （2）NodeJS全局API/bombTimer.js
@@ -1,30 +1,28 @@
 
 
+// me
+// function Bomb(){
+//     this.message="bomb!!!";
+//     this.explode=function(){
+//         console.log(this.message);
+//     }
 
+// }
+// var bomb=new Bomb();
+// setTimeout(function(){
+//     bomb.explode();
+// },2000);
+
+//teacher
 function Bomb(){
-    this.message="bomb!!!";
-    this.explode=function(){
-        console.log(this.message);
-    }
-
+    this.message="bomb!!";
 }
+Bomb.prototype.explode=function(){
+    console.log(this.message);
+}
+
 var bomb=new Bomb();
-setTimeout(function(){
-    bomb.explode();
-},2000);
+setTimeout(bomb.explode.bind(bomb),2000);
 
 
 
-
-
-// var i=0;
-// process.stdin.on("data",function(data){
-//     i++;
-//     if(i==4){
-//         process.exit();
-
-//     }
-//     else{
-//         console.log(data.toString());
-//     }
-// })
--- （2）NodeJS全局API/fileDirName.js
+++ （2）NodeJS全局API/fileDirName.js
@@ -1,7 +1,8 @@
 const http = require("http");
-const fs = require("fs");
+const fs = require("fs");//关于文件模块的引入
 const url = require("url");
-const path = require("path");
+const path = require("path");//兼容性的路径
+//老师的写法：var filepath=path.join(__dirname,"路径")，path.join方法
 var server = http.createServer(function(req, res) {
     var urlObj = url.parse(req.url);
     var urlPathName = urlObj.pathname;
@@ -10,15 +11,21 @@ var server = http.createServer(function(req, res) {
     }
     else if(urlPathName == "/") {
         var htmlPath = __dirname + "\\views\\view.html";
-        var htmlContent = fs.readFileSync(htmlPath);
-        htmlContent = htmlContent.toString("utf8");
+        var htmlContent = fs.readFileSync(htmlPath);//读取文件中的内容
+        htmlContent = htmlContent.toString("utf8");//转化成字符串
         console.log(htmlPath);
         
-        res.writeHead(200, {"Content-Type": "text/html"});
-        res.write(htmlContent);
-        res.end();
+        res.writeHead(200, {"Content-Type": "text/html"});//向客户端响应 头部信息 状态码 类型和内容
+        res.write(htmlContent);//内容
+        res.end();//结束
     }
     
 });
 server.listen(8080);
 console.log("server is listening 8080");
+
+
+
+
+//http.createServer().listen(8080);
+//console.log("");//如果发来请求发生响应的一个是createserver中的函数
